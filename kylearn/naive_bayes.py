067a2a85b7f26edc67da329b2d011f7a6b4ff7e8
Kyle Moore <kyle.m@webenertia.com>
2019-11-18 14:17:00 +0000

    added naive bayes file

---
+++ kylearn/naive_bayes.py
@@ -0,0 +1,17 @@
+"""
+The :mod:`kylearn.naive_bayes` module implements Naive Bayes algorithms. These
+are supervised learning methods based on applying Bayes' theorem with strong
+(naive) feature independence assumptions.
+"""
+
+class MultinomialNB():
+    feature_log_prob = None
+    class_count_ = None
+    feature_count_ = None
+
+
+
+
+if ("__name__" == '__main__):
+    print('hello')
+
